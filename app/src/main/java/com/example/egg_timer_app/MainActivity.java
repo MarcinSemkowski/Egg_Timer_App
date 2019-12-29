7ad8a80e5e995946a96944fd9ab7d88c3c421009
Marcin <marcin96semkowski@gmail.com>
2019-12-29 21:04:06 +0100

    Egg Timer App #1

--- app/src/main/java/com/example/egg_timer_app/MainActivity.java
+++ app/src/main/java/com/example/egg_timer_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.egg_timer_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar = (SeekBar)findViewById(R.id.timerSeekBar);
+
+        timerSeekBar.setMax(600);
+
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
